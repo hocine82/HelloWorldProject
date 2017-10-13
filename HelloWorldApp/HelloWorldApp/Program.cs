7c8132f6e8b6744dbfa9ba78f4734c9fc6fb7d19
hocine82 <hamidoucheh@gmail.com>
2017-10-13 13:06:35 -0400

    Check in Hellow World project

---
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<LogData>();
+            container.RegisterType<IWrite, WriteToDatabase>();
+            container.RegisterType<IWrite, WriteToConsole>();
+            LogData ld = container.Resolve<LogData>();
+            ld.insert();
+        }
+    }
+
+    public class LogData
+    {
+        private IWrite log;
+        public LogData(IWrite write)
+        {
+            log = write;
+        }
+        public bool insert()
+        {          
+            return log.Output();
+        }
+    }
+
+    public interface IWrite
+    {
+        bool Output();
+    }
+
+    public class WriteToConsole : IWrite
+    {
+        public bool Output()
+        {
+            Console.WriteLine("You have chosen to write to console");
+            Console.Read();
+            return true;
+        }
+    }
+
+    public class WriteToDatabase : IWrite
+    {
+        public bool Output()
+        {
+            Console.WriteLine("You have chosen to write to database");
+            Console.Read();
+            return true;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ HelloWorldApp/UnitTest/UnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorldApp;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestConsoleOutput_WillReturnTrue()
+        {            
+            WriteToConsole wc = new WriteToConsole();
+            LogData lg = new LogData(wc);
+            bool isInserted = lg.insert();
+            Assert.AreEqual(isInserted, true);
+        }
+
+        [TestMethod]
+        public void TestDataBaseWrite_WillReturnTrue()
+        {
+            WriteToDatabase wd = new WriteToDatabase();
+            LogData lg = new LogData(wd);
+            bool isInserted = lg.insert();
+            Assert.AreEqual(isInserted, true);
+        }
+
+    }
+}
